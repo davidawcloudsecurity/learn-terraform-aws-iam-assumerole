97617d08382cfd07a9c570a9bf75f18cae47fcd9
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-28 11:06:45 +0800

    Update main.tf - update tag for iam role

--- main.tf
+++ main.tf
@@ -338,9 +338,7 @@ resource "aws_iam_role" "project_trust_platform" {
       }
     ]
   })
-tags = {
-    tag-key = var.example_tag
-  }
+tags = var.example_tag
 }
 
 resource "aws_iam_policy_attachment" "attach_example_policy_01" {
