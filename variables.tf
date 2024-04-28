e476d54076d9473bc3dd07d5d2515a5eee124776
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-28 09:31:39 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "The AWS region where resources will be created."
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "aws_account_id" {
+  description = "The AWS account ID where resources will be created."
+  type        = string
+  default     = "999999999999"
+}
+
+variable "example_tag" {
+  description = "An example tag to apply to resources."
+  type        = map(string)
+  default     = {
+    Name = "example-resource"
+    Environment = "project"
+  }
+}
