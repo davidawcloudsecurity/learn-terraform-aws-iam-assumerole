596077a233bbc33134b77e6127f7fd44e255607b
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-07-19 12:23:35 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = "1.0.11"
-
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.9.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
 }
