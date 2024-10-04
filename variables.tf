9384cc8a0de8818b5fcf8159186cd70846798f50
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-10-04 14:14:23 +0800

    Update variables.tf - added dynamic role to ask for user's input

--- variables.tf
+++ variables.tf
@@ -14,6 +14,11 @@ variable "platform_account_id" {
   type        = string
 }
 
+variable "project_iam_role" {
+  description = "The AWS account ID where resources will be created."
+  type        = string
+}
+
 variable "example_tag" {
   description = "An example tag to apply to resources."
   type        = map(string)
