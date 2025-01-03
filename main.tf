6d560d8ec6172fa13ee2accbf7db92539d878a47
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-01-03 13:21:24 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,36 @@ provider "aws" {
   region = var.aws_region
 }
 
+variable "aws_region" {
+  description = "The AWS region where resources will be created."
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "project_account_id" {
+  description = "The AWS account ID where resources will be created."
+  type        = string
+}
+
+variable "platform_account_id" {
+  description = "The AWS account ID where resources will be created."
+  type        = string
+}
+
+variable "project_iam_role" {
+  description = "The AWS account ID where resources will be created."
+  type        = string
+}
+
+variable "example_tag" {
+  description = "An example tag to apply to resources."
+  type        = map(string)
+  default     = {
+    agency = "example-resource"
+    project = "project"
+  }
+}
+
 # Please check if this matches your platform-policy-new1.json
 resource "aws_iam_policy" "platform_policy_new_01" {
   name        = "policy-1"
